3f86bfec8c2e216e80da0611ede9f485c91872e7
Till <till.nicke@gmail.com>
2020-04-26 10:04:16 +0200

    vectorizer needed for model

--- model/predictor.py
+++ model/predictor.py
@@ -13,49 +13,110 @@ class Predictor():
 		self._load()
 
 		
+	
 	def _load(self):
+		'''
+		initalize the model with a predefined architecture
+		and weights
+		'''
 		try:
-			self.model = keras.models.load_model('weights/GloVe6b-100-LSTM-1.h5')
+			json_file = open('weights/model.json', 'r')
+			loaded_model_json = json_file.read()
+			json_file.close()
+			loaded_model = model_from_json(loaded_model_json)
+
+			loaded_model.load_weights("weights/model_weights.h5")
+			self.model=loaded_model
 		except:
-			# Normal loading did not work. 
-			#print('regular load failed')
-			try:
-				#print('try to load the model from json')
-				json_file = open('weights/model.json', 'r')
-				loaded_model_json = json_file.read()
-				json_file.close()
-				loaded_model = model_from_json(loaded_model_json)
-
-				loaded_model.load_weights("weights/model_weights.h5")
-				self.model=loaded_model
-			except:
-				print('Loading backup weights')
-				# Loading it with json did not work either. 
-				# Only thing that is left is to load the weights with numpy
-				# horrible.. but works
-				
-				json_file = open('../Experiments/GloVe/model.json', 'r')
-				loaded_model_json = json_file.read()
-				json_file.close()
-				loaded_model = model_from_json(loaded_model_json)
+			print('Loading backup weights')
+			self._load_backup()
 				
 
-				embedded = np.load('weights/backup/embedded_weights.npy')
-				lstm_weights = np.load('weights/backup/LSTM_weights.npy', allow_pickle=True)
-				dense_weights = np.load('weights/backup/dense_weights.npy',allow_pickle=True)
-				out_weights = np.load('weights//backupout_weights.npy',allow_pickle=True)
+	
 
-				loaded_model.layers[0].set_weights(embedded)
-				loaded_model.layers[1].set_weights(lstm_weights)
-				loaded_model.layers[4].set_weights(dense_weights)
-				loaded_model.layers[6].set_weights(out_weights)
-				#print(loaded_model.summary())
-				self.model = loaded_model
 
-		#print(self.model.summary())
+	def _load_backup(self):
+		''' 
+		These are backu weights! We might update them once everything is over.
+		But they do not perform to well
+		'''
+
+		#Create an instance of the model architecture
+		json_file = open('weights/model.json', 'r')
+		loaded_model_json = json_file.read()
+		json_file.close()
+		loaded_model = model_from_json(loaded_model_json)
+				
+
+		#load specific weights from np array
+		embedded = np.load('weights/backup/embedded_weights.npy')
+		lstm_weights = np.load('weights/backup/LSTM_weights.npy', allow_pickle=True)
+		dense_weights = np.load('weights/backup/dense_weights.npy',allow_pickle=True)
+		out_weights = np.load('weights//backupout_weights.npy',allow_pickle=True)
+
+		#manually put the weights in their place
+		loaded_model.layers[0].set_weights(embedded)
+		loaded_model.layers[1].set_weights(lstm_weights)
+		loaded_model.layers[4].set_weights(dense_weights)
+		loaded_model.layers[6].set_weights(out_weights)
 		
+		self.model = loaded_model
 		
+
+	
+
+
+	def update_weights(path_to_weights):
+		'''
+		Update the weights of an existing architecture
+		If we have trained the current architecture and got better
+		results, etc.
+		input: path to the weights respectively from the folder
+				of this instance
+		return True if succeded or false if it failed
+		'''
+		try:
+			self.model.load_weights(path_to_weights)
+			return True
+		except:
+			return False
+
+	
+
+
+	def update_model(path_to_model, path_to_weights):
+		'''
+		Update the whole model with new weights.
+		Can be used if we have an instance of the model running
+		and want to deploy a new architecture
+		input: path to the model.json and weights.h5 for the new arch
+				They need to be respectively to the model place
+		return True if secceded and False if failed
+		'''
+
+		try:
+			json_file = open(path_to_model, 'r')
+			loaded_model_json = json_file.read()
+			json_file.close()
+			loaded_model = model_from_json(loaded_model_json)
+
+			loaded_model.load_weights(path_to_weights)
+			self.model=loaded_model
+			return True
+		except:
+			return False
+
+
+	
+
+
 	def analyze(to_test):
+		'''
+		Use the model to analyze a text string and try to 
+		find if it is fake or not
+		input: text string
+		return: prediction of the model
+		'''
 
 		tokenized = self.tokenizer.texts_to_sequence([to_test])
 
